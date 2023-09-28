491070b23b5b86a3acc543dcac580a0829c3a6fb
jj <jasonhoang008@gmail.com>
2023-09-28 11:00:01 -0700

    added deposit function

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+def deposit():
+    while True:
+        amount = input("How much would you like to bet? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+                print("Please enter in a number.")
+    return amount
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+deposit()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
